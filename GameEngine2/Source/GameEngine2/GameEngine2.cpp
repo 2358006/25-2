1d515b72ff04431894e186d2ec559c6d702aaab9
2358006 <2358006@mokwon.ac.kr>
2025-09-11 10:51:03 +0900

    GameEngine2

---
+++ GameEngine2/Source/GameEngine2/GameEngine2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameEngine2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameEngine2, "GameEngine2" );
