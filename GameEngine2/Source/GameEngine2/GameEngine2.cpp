448504b3aa215dd1d3028903691a5bee46229a57
penguinjean0421 <180617834+2358006@users.noreply.github.com>
2025-12-16 16:51:12 +0900

    delete

--- GameEngine2/Source/GameEngine2/GameEngine2.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GameEngine2.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameEngine2, "GameEngine2" );
